4d19283faf19e2547c9dbf062d80276f7d3df78e
SamuelHiroyuki <samduasvezesmortal@gmail.com>
2020-08-30 13:33:13 -0300

    Finalização do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce((accumulator: Balance, transaction: Transaction) => {
+      accumulator[transaction.type] += transaction.value;
+      return accumulator;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    });
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ value, type, title }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      value,
+      type,
+      title
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
